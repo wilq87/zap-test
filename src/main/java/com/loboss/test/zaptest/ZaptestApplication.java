d6ec4ab8acedaa9bc0e2ea9734597eb381399d40
wilq87 <wilq87@gmail.com>
2017-06-20 21:35:33 +0200

    initial project setup

---
+++ src/main/java/com/loboss/test/zaptest/ZaptestApplication.java
@@ -0,0 +1,12 @@
+package com.loboss.test.zaptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaptestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/loboss/test/zaptest/ZaptestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loboss.test.zaptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZaptestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
