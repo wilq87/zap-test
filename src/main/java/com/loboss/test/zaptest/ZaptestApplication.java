c54ecb211355a2f7418a5a783cf42e21d1b49c75
wilq87 <wilq87@gmail.com>
2017-06-20 22:01:26 +0200

    hello world

--- src/main/java/com/loboss/test/zaptest/ZaptestApplication.java
+++ src/main/java/com/loboss/test/zaptest/ZaptestApplication.java
@@ -2,10 +2,16 @@ package com.loboss.test.zaptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class ZaptestApplication {
 
+	    @RequestMapping("/")
+	    String home() {
+		return "Hello World!";
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(ZaptestApplication.class, args);
 	}
