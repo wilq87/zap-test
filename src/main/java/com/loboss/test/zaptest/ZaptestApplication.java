acdd247e8261d87ffb07711c3fa71954cf5eca41
wilq87 <wilq87@gmail.com>
2017-06-21 00:15:39 +0200

    try again

--- src/main/java/com/loboss/test/zaptest/ZaptestApplication.java
+++ src/main/java/com/loboss/test/zaptest/ZaptestApplication.java
@@ -1,12 +1,25 @@
 package com.loboss.test.zaptest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class ZaptestApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ZaptestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZaptestApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZaptestApplication.class);
 	}
 }
